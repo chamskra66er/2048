80a176c63e02a18e2beec69f5cebaf6a9f3a0ed4
Anatoliy Novikov <chamskra66er@gmail.com>
2020-07-21 08:36:22 +0300

    added action for sorted picture

--- Game2048Form/HistoryForm.cs
+++ Game2048Form/HistoryForm.cs
@@ -39,30 +39,81 @@ namespace Game2048Form
                 label.ForeColor = System.Drawing.Color.White;
                 label.Font = new Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
                 panel1.Controls.Add(label);
-                y += label.Height + 3;
+                //y += label.Height + 3;
 
                 Label label1 = new Label();
                 label1.Left = x;
-                label1.Top = y;
+                label1.Top = y+20;
                 label1.Text = item.Score.ToString();
                 label1.AutoSize = true;
                 label1.ForeColor = System.Drawing.Color.White;
                 label1.Font = new Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
                 panel1.Controls.Add(label1);
-                y += label1.Height + 10;
+
                                           
                 
-                if (level>=700)
+                if (level>=7000)
                 {
-                    
+                    CreatePicture(3, true, y);
                 }
-                else
+                else if(level>=6000)
                 {
-
+                    CreatePicture(2, true, y);
+                }
+                else if(level>=5000)
+                {
+                    CreatePicture(1, true, y);
+                }
+                else if(level>=4000)
+                {
+                    CreatePicture(3, false, y);
+                }
+                else if(level>=3000)
+                {
+                    CreatePicture(2, false, y);
+                }
+                else if(level>=2000)
+                {
+                    CreatePicture(1, false, y);
                 }
 
-
+                y += label.Height + 2;
+                y += label1.Height + 10;
             }
+        }
+        private void CreatePicture(int count, bool level, int y)
+        {
+            int x1 = 10;
+
+            if(level)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Left = x1 + 155 + i*30;
+                    pictureBox.Top = y;
+                    pictureBox.Size = new Size(25, 25);
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox.Image = Properties.Resources.korona_50;
+
+                    panel1.Controls.Add(pictureBox);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Left = x1 + 155 + i * 30;
+                    pictureBox.Top = y;
+                    pictureBox.Size = new Size(25, 25);
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox.Image = Properties.Resources.zvezda_50;
+
+                    panel1.Controls.Add(pictureBox);
+                }
+            }           
+           
         }
     }
 }
